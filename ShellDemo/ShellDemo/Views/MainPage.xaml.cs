d5c86d679f11151d05711dff9f8dd3dddb391ce2
Luce Carter <luce.carter@dunnhumby.com>
2019-06-11 08:54:32 +0100

    Translated to Shell for navigation

--- ShellDemo/ShellDemo/Views/MainPage.xaml.cs
+++ ShellDemo/ShellDemo/Views/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using Xamarin.Forms;
 using TabbedPage = Xamarin.Forms.TabbedPage;
 
 namespace ShellDemo
@@ -6,7 +7,7 @@ namespace ShellDemo
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : TabbedPage
+    public partial class MainPage : Shell
     {
         public MainPage()
         {
